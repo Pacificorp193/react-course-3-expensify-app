06fd9fc51de7f18a9dc0e78ac1882b2390084e81
Pacificorp193 <54546134+Pacificorp193@users.noreply.github.com>
2022-06-24 17:11:36 -0600

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 
 const app = firebase.initializeApp(firebaseConfig);
 
-const database = firebase.database(firebase.initializeApp());
+const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export { firebase, googleAuthProvider, database as default };
