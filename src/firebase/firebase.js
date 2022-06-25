d74ec8d391effd8bea6da98a4dd70ade6dade287
Pacificorp193 <54546134+Pacificorp193@users.noreply.github.com>
2022-06-24 18:44:42 -0600

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,16 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyAPZw5IzkroUh_P4B7-FXUESrBZtBbmCr4",
+  authDomain: "gavin-expensify-app.firebaseapp.com",
+  databaseURL: "https://gavin-expensify-app-default-rtdb.firebaseio.com",
+  projectId: "gavin-expensify-app",
+  storageBucket: "gavin-expensify-app.appspot.com",
+  messagingSenderId: "736152791663",
+  appId: "1:736152791663:web:973928a2658fbeaa9a3b9a"
 };
 
-const app = firebase.initializeApp(databaseURL());
+const app = firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
